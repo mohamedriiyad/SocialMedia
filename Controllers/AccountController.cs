bde045a4ab389097dff38643e366a941882aa19b
Mohamed Riyad <mohamedriiyad@gmail.com>
2022-08-22 15:57:42 +0200

    AddAccountController

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SocialMedia.Models;
+using SocialMedia.ViewModels;
+
+namespace SocialMedia.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(ILogger<HomeController> logger,
+            UserManager<IdentityUser> userManager,
+            SignInManager<IdentityUser> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewData["returnUrl"] = returnUrl == null ? "/" : returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model, string returnUrl)
+            {
+            if (!ModelState.IsValid)
+                return View(model); 
+
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, false);
+            if (result.Succeeded)
+                return LocalRedirect(returnUrl?? "/Home/Index");
+            
+            ModelState.AddModelError("", "Failed");
+            return View(model);
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            var user = new IdentityUser
+            {
+                UserName = model.Username,
+                Email = model.Email
+            };
+            
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+                return RedirectToAction("Login");
+            
+            foreach(var error in result.Errors)
+                ModelState.AddModelError(error.Code, error.Description);
+            
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        [Compare("Password", ErrorMessage = "Password and Confrim Password must be the same!")]
+        public string ConfirmPassword { get; set; }
+    }
+}
