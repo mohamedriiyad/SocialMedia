ca783bf430d84e50e47d2f05afd37bf10c7be740
Abdallah Mahmoud <abdullahmahmoud4550@gmail.com>
2022-08-22 14:57:07 +0200

    Add Models

---
+++ Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string CommentBody { get; set; }
+        public string AttachmentURL { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+        public List<Reply> Replies { get; set; }
+    }
+}
---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string PostBody { get; set; }
+        public string AttachmentURL { get; set; }
+        public int UserId { get; set; }
+        public ApplicationUser User { get; set; }
+
+        public List<Comment> comments { get; set; }
+    }
+}
